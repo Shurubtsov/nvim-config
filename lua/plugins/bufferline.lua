a77788b68656f85eed8854f4f9510627f635cc6e
Shurubtsov <dshutubtsov@gmail.com>
2022-12-05 01:49:53 +0300

    mod buffline and lualine

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,6 @@
 require('bufferline').setup {
   options = {
-	close_command = "bdelete! %d",
+	close_command = "bdelete!",
     mode = 'buffers',
 	diagnostics = "nvim_lsp",
     numbers = 'id',
@@ -14,7 +14,15 @@ require('bufferline').setup {
     show_close_icon = false,
     separator_style = 'thin',
     sort_by = 'insert_after_current',
-  }
+	offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				highlight = "Directory",
+				separator = true -- use a "true" to enable the default, or set your own character
+			}
+		}
+	}
 }
 
 local map = vim.api.nvim_set_keymap
@@ -22,3 +30,21 @@ local opts = {noremap = true}
 
 map('n', ']b', '<cmd>BufferLineCycleNext<CR>', opts)
 map('n', '[b', '<cmd>BufferLineCyclePrev<CR>', opts)
+map('n', '<C-b>', '<cmd>bdelete!<CR>', opts)
+
+diagnostics_indicator = function(count, level, diagnostics_dict, context)
+  local icon = level:match("error") and " " or " "
+  return " " .. icon .. count
+end
+
+diagnostics_indicator = function(count, level, diagnostics_dict, context)
+  local s = " "
+  for e, n in pairs(diagnostics_dict) do
+    local sym = e == "error" and " "
+      or (e == "warning" and " " or "" )
+    s = s .. n .. sym
+  end
+  return s
+end
+
+
