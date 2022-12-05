41c690b65f99c102760275d5708f132e87f3fec5
Shurubtsov <dshutubtsov@gmail.com>
2022-12-05 12:27:04 +0300

    add lua lsp and mason packages for lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,6 +10,17 @@ end
 
 local on_attach = require('plugins.lsp')
 
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+require("mason-lspconfig").setup()
+
 lspconfig.gopls.setup(config({
   on_attach = on_attach,
   cmd = {"gopls", "serve"},
@@ -23,6 +34,32 @@ lspconfig.gopls.setup(config({
 		unusedwrite = true,
       },
       staticcheck = true,
+    },
+  },
+}))
+
+lspconfig.sumneko_lua.setup(config({
+  on_attach = on_attach,
+  cmd = {"lua-language-server"},
+  filetypes = {"lua"},
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
     },
   },
 }))
