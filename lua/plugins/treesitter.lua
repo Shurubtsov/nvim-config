b6329b902d1ea313921311de839e9b1cfc29018a
Данила Шурубцов <danila.shurubtsov@bergen.tech>
2023-02-17 11:30:24 +0300

    fix dashboard and add rust treesitter with rust analyzer lsp

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 -- Treesitter is syntax highlighter
 require('nvim-treesitter.configs').setup {
   ensure_installed = { 'go', 'lua', 'make', 'yaml', 'sql', 'proto', 'markdown',
-    'gomod', 'gowork' },
+    'gomod', 'gowork', 'rust' },
   highlight = { enable = true },
 }
