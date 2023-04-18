3bfc9807ded8bb736ef0e154b0e1e0d4b4a91425
Данила Шурубцов <danila.shurubtsov@bergen.tech>
2023-04-18 18:01:27 +0300

    update options

--- lua/options.lua
+++ lua/options.lua
@@ -1,18 +1,19 @@
 -- options for editor --
 local options = {
-	number = true,     -- show number column
-	mouse = 'a',       -- enable mouse in all mods
-	ignorecase = true, -- ignore uppercase in search mode
-	smartcase = true,  -- makes our search ignore uppercase letters unless the search tem has an uppdercase letter.
-	tabstop = 4,       -- amount of spaces in one TAB character
-	shiftwidth = 4,    -- amount of spaces for characters with commands "<<" and ">>"
-	termguicolors = true, -- "best" version of the colorscheme
-	expandtab = true,  -- convert tabs to spaces
-	cursorline = true, -- highlight the current line
+    number = true,               -- show number column
+    mouse = 'a',                 -- enable mouse in all mods
+    ignorecase = true,           -- ignore uppercase in search mode
+    smartcase = true,            -- makes our search ignore uppercase letters unless the search tem has an uppdercase letter.
+    tabstop = 4,                 -- amount of spaces in one TAB character
+    shiftwidth = 4,              -- amount of spaces for characters with commands "<<" and ">>"
+    termguicolors = true,        -- "best" version of the colorscheme
+    expandtab = true,            -- convert tabs to spaces
+    cursorline = true,           -- highlight the current line
+    clipboard = "unnamedplus",   -- allows neovim to access the system clipboard
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 -- disable netrw for nvim-tree
