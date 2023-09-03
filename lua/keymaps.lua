dc78a7ffcdcc391629c3b8066dce7fcd032dcc1a
Данила Шурубцов <dshutubtsov@gmail.com>
2023-09-03 21:50:55 +0300

    add prompt for closing windows

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = ' ' -- main key <space>
-
+-- CTRL + W , c = close current window (not buffer)
+-- CTRL + W, o = close all windows but keep alive current window
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 map("n", "<C-b>", "<cmd>bdelete!<CR>", opts)
-map("n", "<C-w>", "<cmd>bwipeout<CR>", opts)
 map("n", "<C-d>", "<C-d>zz", opts)
 map("n", "<C-u>", "<C-u>zz", opts)
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save' })
