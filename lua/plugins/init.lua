bfe8cbc0126d752e58f88c790005132ca5ebe376
Данила Шурубцов <dshutubtsov@gmail.com>
2023-12-13 16:12:26 +0300

    add extension for telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,6 +11,7 @@ return {
     { "fatih/vim-go" },
     { "ziglang/zig.vim" },
     { "ellisonleao/glow.nvim", config = true, cmd = "Glow" }, -- markdown preview (bin in dir: ~/.local/bin)
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }, -- extension for telescope
     {
         "olexsmir/gopher.nvim",
         dependencies = {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,27 @@
 return {
     "nvim-telescope/telescope.nvim",
-    version = "0.1.x",
-    dependencies = { { "nvim-lua/plenary.nvim" } },
+    branch = "0.1.x",
+    dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-telescope/telescope-fzf-native.nvim" } },
     config = function()
         require('telescope').setup {
             defaults = {
-                file_sorter = require('telescope.sorters').get_fzy_sorter,
                 prompt_prefix = '> ',
                 color_devicons = true,
 
                 file_previewer = require('telescope.previewers').vim_buffer_cat.new,
                 grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
                 qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+            },
+            fzf = {
+                fuzzy = true,                   -- false will only do exact matching
+                override_generic_sorter = true, -- override the generic sorter
+                override_file_sorter = true,    -- override the file sorter
+                case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
             }
         }
+        -- To get fzf loaded and working with telescope, you need to call
+        -- load_extension, somewhere after setup function:
+        require('telescope').load_extension('fzf')
 
         local map = vim.api.nvim_set_keymap
         local opts = { noremap = true, silent = true }
