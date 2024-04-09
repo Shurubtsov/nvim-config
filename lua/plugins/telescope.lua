e81f1dcdc4280cf53647fa42081922a70db195e2
Danila Shurubtsov <dshutubtsov@gmail.com>
2024-04-09 11:05:20 +0300

    change telescope to fzf-lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,11 +7,32 @@ return {
             defaults = {
                 prompt_prefix = '> ',
                 color_devicons = true,
-                file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-                grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-                qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+                -- file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+                -- grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+                -- qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+            },
+            pickers = {
+                live_grep = {
+                    max_results = 6000,
+                    only_sort_text = true,
+                    additional_args = function(opts)
+                        return { '--smart-case', '-u' } -- -u removes duplicate lines
+                    end,
+                    engine = "ripgrep",
+                },
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true,                   -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                }
             },
         }
+        -- To get fzf loaded and working with telescope, you need to call
+        -- load_extension, somewhere after setup function:
+        require('telescope').load_extension('fzf')
+
         local map = vim.api.nvim_set_keymap
         local opts = { noremap = true, silent = true }
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,27 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-        local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-        ts_update()
-    end,
     config = function()
-        -- Treesitter is syntax highlighter
         require 'nvim-treesitter.configs'.setup {
             ensure_installed = { 'go', 'lua', 'make', 'yaml', 'sql', 'proto', 'markdown',
                 'gomod', 'gowork', 'rust', 'vim', 'luadoc', 'zig' },
+            sync_install = false,
+            auto_install = true,
             highlight = { enable = true },
         }
     end
 }
+-- return {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = function()
+--         local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+--         ts_update()
+--     end,
+--     config = function()
+--         -- Treesitter is syntax highlighter
+--         require 'nvim-treesitter.configs'.setup {
+--             ensure_installed = { 'go', 'lua', 'make', 'yaml', 'sql', 'proto', 'markdown',
+--                 'gomod', 'gowork', 'rust', 'vim', 'luadoc', 'zig' },
+--             highlight = { enable = true },
+--         }
+--     end
+-- }
