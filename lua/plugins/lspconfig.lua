755bc61a7844e082e4df9da95dbb36015af850b1
Danila Shurubtsov <dshutubtsov@gmail.com>
2025-10-02 10:28:02 +0300

    upd

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,8 +2,8 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
         -- LSP configuration for autocompletion
-        lspconfig = require "lspconfig"
-        util = require "lspconfig/util"
+        local lspconfig = require "lspconfig"
+        local util = require "lspconfig/util"
 
         local function config(_config)
             return vim.tbl_deep_extend('force', {
@@ -17,7 +17,7 @@ return {
         lspconfig.gopls.setup(config({
             on_attach = on_attach,
             cmd = { "gopls", "serve" },
-            filetypes = { "go", "gomod" },
+            filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
             root_dir = util.root_pattern("go.work", "go.mod", ".git"),
             settings = {
                 gopls = {
@@ -37,7 +37,7 @@ return {
         -- LSP for Rust
         lspconfig.rust_analyzer.setup(config({
             on_attach = on_attach,
-        cmd = { "rust-analyzer" },
+            cmd = { "rust-analyzer" },
             filetypes = { "rust", "rs" },
             root_dir = util.root_pattern("Cargo.toml", "rust-project.json"),
             settings = {
@@ -64,46 +64,56 @@ return {
         lspconfig.lua_ls.setup(config({
             on_attach = on_attach,
             on_init = function(client)
-                local path = client.workspace_folders[1].name
-                if not vim.uv.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-                    client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-                        Lua = {
-                            runtime = {
-                                -- Tell the language server which version of Lua you're using
-                                -- (most likely LuaJIT in the case of Neovim)
-                                version = 'LuaJIT'
-                            },
-                            -- Make the server aware of Neovim runtime files
-                            workspace = {
-                                checkThirdParty = false,
-                                library = {
-                                    vim.env.VIMRUNTIME,
-                                    "${3rd}/luv/library",
-                                    -- "${3rd}/busted/library",
-                                }
-                                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                                -- library = vim.api.nvim_get_runtime_file("", true)
-                            },
-                            diagnostics = {
-                                -- Get the language server to recognize the `vim` global
-                                globals = { 'vim' },
-                            },
-                            -- Do not send telemetry data containing a randomized but unique identifier
-                            telemetry = {
-                                enable = false,
-                            },
-                        }
-                    })
-
-                    client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                local uv = vim.uv or vim.loop
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if
+                        path ~= vim.fn.stdpath('config')
+                        and (uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+                    then
+                        return
+                    end
                 end
-                return true
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using (most
+                        -- likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT',
+                        -- Tell the language server how to find Lua modules same way as Neovim
+                        -- (see `:h lua-module-load`)
+                        path = {
+                            'lua/?.lua',
+                            'lua/?/init.lua',
+                        },
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                            -- Depending on the usage, you might want to add additional paths
+                            -- here.
+                            -- '${3rd}/luv/library'
+                            -- '${3rd}/busted/library'
+                        }
+                        -- Or pull in all of 'runtimepath'.
+                        -- NOTE: this is a lot slower and will cause issues when working on
+                        -- your own configuration.
+                        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+                        -- library = {
+                        --   vim.api.nvim_get_runtime_file('', true),
+                        -- }
+                    }
+                })
             end,
-            cmd = { "lua-language-server" },
-            filetypes = { "lua" },
-            root_dir = util.root_pattern(".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml",
-                "selene.toml", "selene.yml", ".git"),
-            log_level = 2
+            -- cmd = { "lua-language-server" },
+            -- filetypes = { "lua" },
+            -- root_dir = util.root_pattern(".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml",
+            --     "selene.toml", "selene.yml", ".git"),
+            -- log_level = 2
+            settings = {
+                Lua = {}
+            }
         }))
 
         -- LSP for C, C++ etc.
